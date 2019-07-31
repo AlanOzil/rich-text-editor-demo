6df14b8111e83bf87bbb0498b17090f3e14f3818
unknown <wb.wangzhengrui@mesg.corp.netease.com>
2019-07-31 14:24:13 +0800

    'init'

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # tinymce-demo
 
+> tinymce富文本插件的demo
+
+## Tinymce Configuration
+See [https://www.tiny.cloud/docs/](https://www.tiny.cloud/docs/).
+
+### Advantage
+- [x] 前后端分离
+- [x] 开发者持续更新
+- [x] 对图片上传的支持较好
+
 ## Project setup
 ```
 npm install
--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,77 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div id="app">
+  <editor id="editor" v-model="tinymceHtml" :init="editorInit"></editor>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import tinymce from 'tinymce/tinymce'
+import 'tinymce/themes/silver'
+import 'tinymce/skins/ui/oxide/skin.min.css'
+import Editor from '@tinymce/tinymce-vue'
+// 载入插件
+import 'tinymce/plugins/image'
+import 'tinymce/plugins/imagetools'
+import 'tinymce/plugins/link'
+import 'tinymce/plugins/paste'
+import 'tinymce/plugins/fullscreen'
+import 'tinymce/plugins/preview'
+const __IMG_UPLOAD_URL__ = 'https://sm.ms/api/upload'
 export default {
   name: 'app',
+  data() {
+    return {
+      tinymceHtml: '',
+      editorInit: {
+        language_url: `/tinymce/langs/zh_CN.js`,
+        language: 'zh_CN',
+        height: 300,
+        branding: false, // 去水印
+        elementpath: false, //禁用编辑器底部的状态栏
+        statusbar: false, // 隐藏编辑器底部的状态栏
+        paste_data_images: true, // 允许粘贴图像
+        plugins: 'image imagetools link paste fullscreen preview',
+        images_upload_url: __IMG_UPLOAD_URL__,
+        // override default upload handler to simulate successful upload
+        images_upload_handler: function(blobInfo, success, failure) {
+          var xhr, formData
+          xhr = new XMLHttpRequest()
+          xhr.withCredentials = false
+          xhr.open('POST', __IMG_UPLOAD_URL__)
+
+          xhr.onload = function() {
+            var json
+            if (xhr.status != 200) {
+              failure('HTTP Error: ' + xhr.status)
+              alert('图片上传失败')
+              return
+            }
+            json = JSON.parse(xhr.responseText)
+            if (!json || !json.data || typeof json.data.url != 'string') {
+              failure('Invalid JSON: ' + xhr.responseText)
+              alert('图片上传失败')
+              return
+            }
+            success(json.data.url) //用服务器地址替换img的src
+          }
+          formData = new FormData()
+          formData.set('smfile', blobInfo.blob())
+          xhr.send(formData)
+        }
+      }
+    }
+  },
   components: {
-    HelloWorld
+    Editor
+  },
+  mounted() {
+    tinymce.init({
+      ...this.editorInit,
+      selector: '#editor',
+      theme: 'silver',
+      skin: "oxide",
+      skin_url: '/tinymce/skins/ui/oxide'
+    })
   }
 }
 </script>
